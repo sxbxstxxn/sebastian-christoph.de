9a0e81d0bffc4b65e955e9adc882dc3f36d5922f
root <root@helden.online>
2026-05-21 09:27:36 +0000

    some stuff

--- sebastian_blog/settings/dev.py
+++ sebastian_blog/settings/dev.py
@@ -1,3 +1,10 @@
 from .base import *
 DEBUG = True
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+CSRF_TRUSTED_ORIGINS = [
+            "https://sebastian-christoph.de",
+                "https://www.sebastian-christoph.de",
+                ]
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
