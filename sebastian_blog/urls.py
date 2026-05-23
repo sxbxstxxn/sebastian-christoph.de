f223fe0725cf8cf9acf6459e1751a16328a5a98c
sxbxstxxn <sxbxstxxn@googlemail.com>
2026-05-23 09:18:14 +0200

    add impressum

--- sebastian_blog/urls.py
+++ sebastian_blog/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
@@ -10,6 +11,11 @@ urlpatterns = [
     path("django-admin/", admin.site.urls),
     path("admin/", include(wagtailadmin_urls)),
     path("documents/", include(wagtaildocs_urls)),
+    path(
+        "impressum/",
+        TemplateView.as_view(template_name="content/impressum.html"),
+        name="impressum",
+    ),
 ]
 
 if settings.DEBUG:
