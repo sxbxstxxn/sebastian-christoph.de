fbba98f36bb08c5ecf41fdbb6a47acd8428d1228
sxbxstxxn <sxbxstxxn@googlemail.com>
2026-06-03 10:17:06 +0200

    fix

--- sebastian_blog/urls.py
+++ sebastian_blog/urls.py
@@ -1,7 +1,8 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.static import serve
 from django.views.generic import TemplateView
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail import urls as wagtail_urls
@@ -22,5 +23,13 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+elif settings.SERVE_MEDIA:
+    urlpatterns += [
+        re_path(
+            r"^media/(?P<path>.*)$",
+            serve,
+            {"document_root": settings.MEDIA_ROOT},
+        )
+    ]
 
 urlpatterns += [path("", include(wagtail_urls))]
