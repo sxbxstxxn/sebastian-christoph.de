037e71bb6a8fd766a89067efd2762bad180d6f3b
sxbxstxxn <sxbxstxxn@googlemail.com>
2026-06-07 00:26:51 +0200

    fb beitrags id entfernen button

--- content/wagtail_hooks.py
+++ content/wagtail_hooks.py
@@ -1,4 +1,9 @@
+from django.contrib import messages
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import path, reverse
 from wagtail import hooks
+from wagtail.admin.action_menu import ActionMenuItem
+from wagtail.admin.auth import require_admin_access
 
 from .models import BaseContentPage
 from .social import (
@@ -7,6 +12,66 @@ from .social import (
     publish_page_to_facebook,
     publish_page_to_instagram,
 )
+
+
+class ResetFacebookPostIdMenuItem(ActionMenuItem):
+    name = "reset-facebook-post-id"
+    label = "Facebook-ID zuruecksetzen"
+
+    def get_url(self, context):
+        page = context.get("page")
+
+        if not page:
+            return None
+
+        return reverse("content_admin_reset_facebook_post_id", args=[page.id])
+
+    def is_shown(self, context):
+        page = context.get("page")
+
+        if not page:
+            return False
+
+        specific_page = page.specific
+        return isinstance(specific_page, BaseContentPage) and bool(specific_page.facebook_post_id)
+
+
+@hooks.register("register_page_action_menu_item")
+def register_reset_facebook_post_id_menu_item():
+    return ResetFacebookPostIdMenuItem(order=95)
+
+
+@hooks.register("register_admin_urls")
+def register_social_admin_urls():
+    return [
+        path(
+            "pages/<int:page_id>/reset-facebook-post-id/",
+            reset_facebook_post_id,
+            name="content_admin_reset_facebook_post_id",
+        ),
+    ]
+
+
+@require_admin_access
+def reset_facebook_post_id(request, page_id):
+    page = get_object_or_404(BaseContentPage, pk=page_id)
+
+    if request.method == "POST":
+        BaseContentPage.objects.filter(pk=page_id).update(
+            facebook_post_id="",
+            facebook_last_error="",
+        )
+        messages.success(
+            request,
+            "Die Facebook Post-ID wurde zurueckgesetzt. Beim naechsten Veroeffentlichen kann der Beitrag erneut gepostet werden.",
+        )
+        return redirect("wagtailadmin_pages:edit", page_id)
+
+    return render(
+        request,
+        "content/admin/reset_facebook_post_id.html",
+        {"page": page},
+    )
 
 
 @hooks.register("after_publish_page")
